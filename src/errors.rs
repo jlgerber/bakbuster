928c5668cd8ba998e57e06597f4579e7c44b3511
jonathan gerber <jlgerber@gmail.com>
2018-11-25 05:51:46 -0800

    add ConversionError

--- src/errors.rs
+++ src/errors.rs
@@ -1,17 +1,20 @@
 #[derive(Fail, Debug,PartialEq,Eq, PartialOrd, Ord)]
 pub enum BBError {
     /// Parsing error
-    #[fail(display = "failed to parse: {}", _0)]
+    #[fail(display = "ParseError: failed to parse: {}", _0)]
     ParseError(String),
     /// General Errors originating in the Reqwest module
     /// Error raised when a feature is not implemented yet
     #[fail(display = "NotImplemented")]
     NotImplemented,
     /// Failure to find look up the specified variable in the environment
-    #[fail(display = "{} not found in environment", _0)]
+    #[fail(display = "EnvVarError: {} not found in environment", _0)]
     EnvVarError(String),
      /// path provided does not exist
-    #[fail(display = "{} does not exist", _0)]
+    #[fail(display = "NonExtantPath: {} does not exist", _0)]
     NonExtantPath(String),
+    #[fail(display = "ConversionError: {}", _0)]
+    ConversionError(String),
+
 
 }
