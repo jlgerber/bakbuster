52f1a9bdd484ddca62db59e264390933c1cfbfe2
jonathan gerber <jlgerber@gmail.com>
2018-11-25 07:03:28 -0800

    add constants with time formats

---
+++ src/constants.rs
@@ -0,0 +1,4 @@
+// Common format like ```2018-10-11 09:32:00```
+pub static TIMEFMT: &'static str = "%Y-%m-%d %H:%M:%S";
+// The format that bak dir uses
+pub static BAKTIMEFMT: &'static str = "%Y%m%d-%H%M%S";
